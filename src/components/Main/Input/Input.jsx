99b0f92b3bf3e796d069f81564379d9fe4345a71
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2023-04-09 14:21:28 -0600

    Avances

---
+++ src/components/Main/Input/Input.jsx
@@ -0,0 +1,76 @@
+import React from "react"
+import {
+  NavBar,
+  DivBotonRespuesta,
+  BotonRespuesta,
+  DivTextArea,
+  BotonTextArea,
+  TextDerechosGPT,
+} from "./Input.module.css"
+
+const Input = () => {
+  return (
+    <div className={NavBar}>
+      <form>
+        <div>
+          <div className={DivBotonRespuesta}>
+            <button type="button" className={BotonRespuesta}>
+              <div>
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="1.5"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <polyline points="1 4 1 10 7 10" />
+                  <polyline points="23 20 23 14 17 14" />
+                  <path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15" />
+                </svg>
+                <span>Regenerate response</span>
+              </div>
+            </button>
+          </div>
+          <div className={DivTextArea}>
+            <textarea placeholder="Send a message..." />
+            <button type="button" className={BotonTextArea}>
+              <svg
+                stroke="currentColor"
+                fill="none"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line x1="22" y1="2" x2="11" y2="13" />
+                <polygon points="22 2 15 22 11 13 2 9 22 2" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className={TextDerechosGPT}>
+        <span>
+          <a
+            href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            ChatGPT Mar 23 Version
+          </a>
+          . Free Research Preview. ChatGPT may produce inaccurate information
+          about people, places, or facts
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Input
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Main } from "./Main.module.css"
 import DivRespuesta from "./Respuestas/Respuesta"
+import Input from "./Input/Input"
 
 const DivMain = () => {
   return (
     <div className={Main}>
       <DivRespuesta />
+      <Input />
     </div>
   )
 }
